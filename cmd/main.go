6d4e8027fedac876595b4720aba7a032b21bd446
Lee <lee10202013@gmail.com>
2020-11-24 22:28:42 +0800

    add repl

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
 
 	"github.com/chzyer/readline"
 )
 
 func main() {
-	l, err := readline.NewEx(&readline.Config{})
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:          "\033[31m#\033[0m ",
+		HistoryFile:     "/tmp/readline.tmp",
+		InterruptPrompt: "^C",
+	})
 	if err != nil {
 		panic(err)
 	}
@@ -15,4 +20,23 @@ func main() {
 	defer l.Close()
 
 	fmt.Println("This is Goldfoyle.")
+
+repl:
+	for {
+		line, err := l.Readline()
+		if err == readline.ErrInterrupt {
+			if len(line) == 0 {
+				break
+			} else {
+				continue
+			}
+		} else if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Println("Error while reading line:", err)
+			continue repl
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/leewei05/go-sql
 
 go 1.13
 
-require github.com/spf13/cobra v1.1.1 // indirect
+require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/spf13/cobra v1.1.1 // indirect
+)
